d0b9c34b935aba3ae79eb3daafec3c85474d22f4
Sergey <sspopovdev@gmail.com>
2019-04-10 23:48:54 +0300

    add grouping numbers from settled level

--- src/com/home/hailstone/Hailstone.java
+++ src/com/home/hailstone/Hailstone.java
@@ -11,11 +11,22 @@ public class Hailstone {
     private void run() {
         int[] ints = IntStream.iterate(
                 7,
-                x -> cut(3 * x  + 1))
+                x -> cut(3 * x + 1))
                 .limit(100)
                 .filter(x -> x > 1)
                 .toArray();
         System.out.println(Arrays.toString(ints));
+        System.out.println(ints.length);
+
+        int[] ints1 = stepBackFromWithLimit(1, 20);
+        System.out.println(Arrays.toString(ints1));
+        System.out.println(ints1.length);
+
+        int[] ints2 = step(1, 3, 1)
+                .sorted()
+                .toArray();
+        System.out.println(Arrays.toString(ints2));
+        System.out.println(ints2.length);
     }
 
     private int cut(int x) {
@@ -29,5 +40,30 @@ public class Hailstone {
             return cut(x >> 1);
         }
         return x;
+    }
+
+    private int stepBackFrom(int base, int shift) {
+        int denominator = base * (1 << shift) - 1;
+        if (denominator % 3 != 0) {
+            return -1;
+        }
+        return denominator / 3;
+    }
+
+    private int[] stepBackFromWithLimit(int base, int limit) {
+        return IntStream.range(0, limit)
+                .map(x -> stepBackFrom(base, x))
+                .filter(x -> x > 0)
+                .toArray();
+    }
+
+    private IntStream step(int base, int limit, int level) {
+        IntStream intStream = IntStream.range(0, 20)
+                .map(x -> stepBackFrom(base, x))
+                .filter(x -> x > 0);
+        if (level == 0) {
+            return intStream;
+        }
+        return intStream.flatMap(x -> step(x, limit, level - 1));
     }
 }
